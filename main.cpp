39896f22c7e1c9926a739de372e33914502618d6
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2016-07-13 16:37:27 +0200

    Stimulating the pomodoro and showing some result on the ncurses windows

--- main.cpp
+++ main.cpp
@@ -27,12 +27,30 @@ int main()
 	pomotimer::Config config(10,2,4,2); // short times, for testing
 	auto * tomato = new pomotimer::Pomotimer(config);
 	cli_init();
+	printw( "time before start: %u\n", tomato->getTime() );
 	tomato->start();
+	printw( "time at start: %u\n", tomato->getTime() );
 	sleep( 5 );
+	printw( "time after 5s: %u\n", tomato->getTime() );
+	refresh();
 	tomato->stop();
+	sleep( 1 );
+	printw( "time after stop: %u\n", tomato->getTime() );
+	refresh();
+	printw( "time before start: %u\n", tomato->getTime() );
+	tomato->start();
+	printw( "time at start: %u\n", tomato->getTime() );
+	sleep( 5 );
+	printw( "time after 5s: %u\n", tomato->getTime() );
+	refresh();
+	tomato->pause();
+	sleep( 1 );
+	printw( "time after pause: %u\n", tomato->getTime() );
+	refresh();
 	delete tomato;
 // 	while( tomato.getTimerType() != pomotimer::Timer::LONG_BREAK ) {
 // 	}
+	getch();
 	cli_close();
 	return 0;
 }
