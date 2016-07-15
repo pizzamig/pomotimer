fbd3b1641677d31e2cc09a4fe037cb2cbf387184
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2016-07-15 17:27:40 +0200

    Adding tons of documentation

--- pomotimer.h
+++ pomotimer.h
@@ -41,7 +41,8 @@ enum class TimerType : uint8_t { FOCUS, SHORT_BREAK, LONG_BREAK };
 /** The Pomodoro timer state machine.
  * This class implements the state machine under a pomodoro timer
  * It tracks the status of the timer; to update the interal status
- * the update() member function has to be called every second
+ * the update() member function has to be called every second.
+ * All methods are thread-safe
  */
 class Pomodoro {
 public:
@@ -64,13 +65,14 @@ public:
 	 * ( timer set to Focus, time resetted, loop counter resetted )
 	 */
 	void reset();
-	/** Get the left time of the current timer (in seconds)
+	/** Get the left time of the current timer (in seconds).
 	 * This function return the amount of seconds remaining in the current timer
 	 */
 	uint32_t getTime() const { return time; }
-	/** Get the current timer type
+	/** Get the current timer type.
 	 */
 	TimerType getTimerType() const { return type; }
+
 private:
 	TimerType type;
 	uint32_t time;
@@ -79,32 +81,80 @@ private:
 	std::mutex mtx;
 };
 
+/** The Pomodoro timer.
+ * This class represents a complete pomodoro timer.
+ * It provides two observer interface
+ * * when TimerType changes
+ * * when the remaining time changes
+ * It has a basic control interface with start(). stop() and pause() functions.
+ * All methods are thread-safe.
+ * This implementation is fully based on POSIX: it uses SIGEV_THREAD signalling mechanisms and
+ * pthread_* class of functions
+ * The SIGEV_THREAD signalling mechanism arms a periodic timer that, when expired, wake up the timer thread
+ * The timer thread, then, updates the Pomodoro and, in case, send event notifications
+ */
 class Pomotimer : public utility::Observable< TimerType >,
 									public utility::Observable< uint32_t >
 {
 public:
-	Pomotimer( Config & );
+	/** The constructor.
+	 * The constructor initialize all timer elements and it needs only the pomodoro configuration
+	 * @param c The pomodoro configuration
+	 */
+	Pomotimer( Config & c );
+	/** The clean-up destructor.
+	 * The destructor is needed to clean-up all activities that were going on.
+	 * In particular, the desctructor waits that the main thread exits.
+	 */
 	~Pomotimer();
+	/** Start or restart the timer.
+	 * This function starts the pomodoro timer.
+	 * There's no function difference between start and restart.
+	 */
 	void start();
+	/** Stop the timer.
+	 * This function stops the pomodoro timer, bringing it back to its initial status.
+	 * stop() implictely invokes a pomodoro reset
+	 */
 	void stop();
+	/** Pause the timer.
+	 * This function stops the pomodoro timer, but leaving the status untouched.
+	 * No pomodoro reset occur in this case
+	 */
 	void pause();
+	/** Get the left time of the current timer (in seconds).
+	 * This function return the amount of seconds remaining in the current timer
+	 */
 	uint32_t getTime() const { return pomo.getTime(); }
+
 	using utility::Observable< TimerType >::addObs;
 	using utility::Observable< uint32_t >::addObs;
+
 private:
 	using utility::Observable< TimerType >::notifyAllObs;
 	using utility::Observable< uint32_t >::notifyAllObs;
+	/** The thread commanding the timer.
+	 * This is the main thread, taking care of activating the internal timer and so on.
+	 * In general, this thread waits for a command and then execute it, arming or disarming the sigev_thread
+	 */
 	static void* mainThread( void * );
+	/** The timer thread
+	 * This is the thread executed when the SIGEV_THREAD timer expires
+	 * It's responsible to update the Pomodoro status calling update()
+	 * After the update, it's responsible to manage all registered notifications
+	 */
 	static void timerThread( union sigval si );
+	
 	enum Status { RUN, STOP, PAUSE, EXIT };
-	Status is; // internal status
-	Status ns; // new status
+	Status is; //! The current internal status
+	Status ns; //! The new status, issued by functions like start(), stop() or pause()
 	Config & conf;
 	Pomodoro pomo;
+	// pthread stuff
 	pthread_t tid;
 	pthread_mutex_t mutex;
 	pthread_cond_t cond;
-	// sigevent section
+	// SIGEV_THREAD section
 	timer_t timerId;
 	struct itimerspec ts;
 	struct sigevent se;
