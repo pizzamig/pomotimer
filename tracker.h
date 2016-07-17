f35769a2fc7f10a663c7bc37daede81c380d4cec
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2016-07-17 22:46:56 +0200

    Adding documentation to tracker.h

--- tracker.h
+++ tracker.h
@@ -10,11 +10,30 @@ namespace pomotimer {
 	 */
 	class Tracker : public utility::Observer< TimerType > {
 	public:
+		/** The initializing constructor.
+		 * The constructor initilize the internal counters and register itself
+		 * as an observer of timer type events
+		 */
 		Tracker(Pomotimer &p);
+		/** The number of started pomos.
+		 * This function get the number of observer pomodoros that are started
+		 * It's thread safe.
+		 */
 		uint32_t getStartedPomo();
+		/** The number of completed pomos.
+		 * This function get the number of observer pomodoros that are completed
+		 * It's thread safe.
+		 */
 		uint32_t getFinishedPomo();
-		virtual void notify( TimerType t );
+		/** Resetting the counters.
+		 * This functions reset all timers.
+		 * It's thread safe.
+		 */
 		void reset();
+		/** The observer function
+		 * The function invoked by the observable, monitoring the activity of the timer
+		 */
+		virtual void notify( TimerType t );
 	private:
 		Pomotimer & pt;
 		// counters
