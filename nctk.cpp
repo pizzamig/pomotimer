4883f6cc72f989c0377e705e575a4f2d6a418e5c
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2016-08-02 23:23:33 +0200

    Adding several features on nctk
    Application can manage a defined minimum size:
    * added a new constructor
    * ncurses initialization refactored
    Added the size class
    Added nctk exception class
    Added errors enum class
    Added Fatal class, to clean ncurses if the application cannot start

--- nctk.cpp
+++ nctk.cpp
@@ -1,18 +1,75 @@
 #include "nctk.h"
+#include <sstream>
 
-nctk::Application::Application() : rows(0), cols(0)
+std::ostream&
+operator<<(std::ostream& output, const nctk::Errors e)
+{
+	switch( e ) {
+		case nctk::Errors::SIZE_TOO_SMALL:
+			output << "Size too small";
+			break;
+		default:
+			output << "Error type not supported";
+	}
+	return output;
+}
+
+nctk::Exception::Exception(nctk::Errors e) :
+	std::exception(), e(e)
+{
+	std::ostringstream oss;
+	oss << "nctk Exception: " << e ;
+	msg = oss.str();
+}
+
+const char*
+nctk::Exception::what() const noexcept
+{
+	return msg.c_str();
+}
+
+nctk::Size::Size(int lines, int cols) : lines(lines), cols(cols)
+{}
+
+static const nctk::Exception smallSize(nctk::Errors::SIZE_TOO_SMALL);
+
+void
+nctk::Application::init()
 {
 	initscr();
 	cbreak(); // we can use raw(), but we have to implement the signal handling
 	keypad(stdscr, TRUE);
 	noecho();
 	curs_set(0);
-	getmaxyx(stdscr, rows, cols);
+	rows=LINES;
+	cols=COLS;
 	refresh();
+}
+
+nctk::Application::Application() : rows(0), cols(0)
+{
+	init();
+}
+
+nctk::Application::Application(const Size & minSize) : rows(0), cols(0)
+{
+	init();
+	if ( LINES < minSize.getLines() || 
+		COLS < minSize.getCols() ) {
+		throw smallSize;
+	}
 }
 
 nctk::Application::~Application()
 {
+	echo();
+	endwin();
+}
+
+nctk::Fatal::Fatal(nctk::Exception & e)
+{
+	printw("%s - press a key to exit", e.what()); refresh();
+	getch();
 	echo();
 	endwin();
 }
--- nctk.h
+++ nctk.h
@@ -5,6 +5,8 @@
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <exception>
+#include <string>
 
 namespace nctk {
 #if 0
@@ -21,19 +23,47 @@ namespace nctk {
 		std::deque<char> keys;
 	};
 #endif
+enum class Errors : uint16_t { SIZE_TOO_SMALL };
+class Exception : public std::exception
+{
+public:
+	Exception(Errors e);
+	virtual const char* what() const noexcept;
+private:
+	Errors e;
+	std::string msg;
+};
+
+class Size
+{
+public:
+	Size(int lines, int cols);
+	int getCols() const { return cols; }
+	int getLines() const { return lines; }
+private:
+	int lines;
+	int cols;
+};
+
 class Application
 {
 public:
 	Application();
+	Application(const Size & minSize);
 	~Application();
 	int getRows() const { return rows; }
 	int getCols() const { return cols; }
 private:
-
+	void init();
 	int rows;
 	int cols;
 };
 
+class Fatal
+{
+public:
+	Fatal(Exception & e);
+};
 #if 0
 class Window
 {
@@ -44,3 +74,8 @@ private:
 };
 #endif
 } // namespace nctk
+
+#include <ostream>
+
+std::ostream&
+operator<<(std::ostream& output, const nctk::Errors e);
